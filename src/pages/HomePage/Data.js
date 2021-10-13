ff07f2a34aa50dbcfb9b78ff9729fc712d5d478a
Farhaz Khan <farhaz.khan@GIPL.INET>
2021-10-13 14:36:14 +0530

    mmit

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,15 @@
+export const homeObjOne = {
+    primary: true,
+    lightBg: false,
+    imgStart: '',
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Get Started',
+    description: 'We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goal.',
+    headline: 'Lead Generation Specialist for Online Business',
+    lightText: true,
+    topLine: 'Marketing Agency',
+    img: require ('../../images/svg-1.svg'),
+    alt: 'Image',
+    start: ''
+};
